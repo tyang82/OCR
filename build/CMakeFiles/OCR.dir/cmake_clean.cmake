acc5e13c887b20c511fb4f2386d53c10c44a3c8c
tyang82 <tyang82@gatech.edu>
2016-08-02 13:51:46 -0400

    erase noise

---
+++ build/CMakeFiles/OCR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OCR.dir/test.cpp.o"
+  "OCR.pdb"
+  "OCR"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OCR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
